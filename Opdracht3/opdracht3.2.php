fa118305d6a53ba739d1c3c458ed14211bea427b
Namara27 <namara.kerkenaar@student.rocvf.nl>
2019-09-20 10:27:21 +0200

    opdracht 3.2 toegevoegd en img

---
+++ Opdracht3/opdracht3.2.php
@@ -0,0 +1,20 @@
+<html lang="en">
+<body>
+<style>
+    body {
+        text-align: center;
+    }
+</style>
+<?php
+
+for ($i = 0; $i <= 12; $i++) {
+    for ($j = 0; $j < $i; $j++) {
+        echo "*";
+    }
+    echo "*<br>";
+}
+?>
+</body>
+</html>
+
+
