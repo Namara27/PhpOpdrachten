81fe359ddbe955b2f7a3c243756dbc03f5847edf
Namara27 <namara.kerkenaar@student.rocvf.nl>
2019-09-20 08:39:04 +0200

    opdracht 1 teogevoegd

---
+++ Opdracht2.php
@@ -0,0 +1,32 @@
+<?php
+$a = "Ik heb niet gefaald.";
+$b = "die niet werken.";
+$c = "10000";
+$volledig = $a . " Ik heb alleen " . $c . " manieren gevonden " . $b;
+echo $volledig;
+
+echo "<br>";
+
+$e = " die geen fout";
+$f = "en ";
+$g = " niet";
+$volledigeZin = "Mensen " . $e . $f . "maken, " . "werken" . $g . ".";
+echo$volledigeZin;
+
+echo "<br>";
+
+$h = "ry";
+$i = ", but";
+$j = " fail";
+$volZin = "T" . $h . " and" . $j . $i . " never" . $j . " to t" . $h . ".";
+echo $volZin;
+
+echo "<br>";
+
+$k = "\"omgaan";
+$l = " met";
+$m = " teleu";
+$heleZin = "De cursus " . $k . $l . $m . "rstellingen\"" . " kan vanavond helaas niet doorgaan.";
+echo $heleZin;
+
+
