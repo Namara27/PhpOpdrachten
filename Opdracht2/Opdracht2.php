c561ed4409f1ce307dac575e70ebb3b86448340b
Namara27 <namara.kerkenaar@student.rocvf.nl>
2019-09-20 09:14:21 +0200

    opdracht 2 breaks veranderd

--- Opdracht2/Opdracht2.php
+++ Opdracht2/Opdracht2.php
@@ -3,30 +3,22 @@ $a = "Ik heb niet gefaald.";
 $b = "die niet werken.";
 $c = "10000";
 $volledig = $a . " Ik heb alleen " . $c . " manieren gevonden " . $b;
-echo $volledig;
-
-echo "<br>";
+echo $volledig."<br>";
 
 $e = " die geen fout";
 $f = "en ";
 $g = " niet";
 $volledigeZin = "Mensen " . $e . $f . "maken, " . "werken" . $g . ".";
-echo$volledigeZin;
-
-echo "<br>";
+echo$volledigeZin."<br>";
 
 $h = "ry";
 $i = ", but";
 $j = " fail";
 $volZin = "T" . $h . " and" . $j . $i . " never" . $j . " to t" . $h . ".";
-echo $volZin;
-
-echo "<br>";
+echo $volZin."<br>";
 
 $k = "\"omgaan";
 $l = " met";
 $m = " teleu";
 $heleZin = "De cursus " . $k . $l . $m . "rstellingen\"" . " kan vanavond helaas niet doorgaan.";
 echo $heleZin;
-
-
