6df35dcbaede39eac66e60d9c8b1b279a5696145
Namara27 <namara.kerkenaar@student.rocvf.nl>
2019-09-23 13:09:10 +0200

    opdracht 5.1 updated

--- Opdracht5/opdracht5.1.php
+++ Opdracht5/opdracht5.1.php
@@ -1,15 +1,15 @@
 <?php
 
 if ($_POST['naam'] == "") {
-    echo "Je moet nog een naam invullen" ."<br>";
+    echo "Je moet nog een naam invullen<br>";
 }
 if ($_POST['adres'] == "") {
-    echo "Je moet nog een adres invullen" ."<br>";
+    echo "Je moet nog een adres invullen<br>";
 }
 if ($_POST['email'] == "") {
-    echo "Je moet nog een E-mail invullen" ."<br>";
+    echo "Je moet nog een E-mail invullen<br>";
 }
 if ($_POST['wachtwoord'] == "") {
-    echo "Je moet nog een wachtwoord invullen" ."<br>";
+    echo "Je moet nog een wachtwoord invullen<br>";
     echo "<a href=\"opdracht5.1.html\">Terug naar formulier</a>";
 }
