b9a91fd45964f319f08e3f92577363a2cb3c3765
Namara27 <namara.kerkenaar@student.rocvf.nl>
2019-10-01 12:27:21 +0200

    removed foreach loop and test msgs

--- Opdracht8/index.php
+++ Opdracht8/index.php
@@ -4,12 +4,6 @@ include_once('lib/Radioprogramma.php');
 include_once('lib/Liedje.php');
 
 $ditprogramma = new Radioprogramma();
-$ditprogramma->setProgrammaNaam("test");
-$ditprogramma->setOmschrijving("TeSTTTTTTT");
-
-foreach ($ditprogramma->getRadioprogramma() as $p) {
-    echo $p . "<br>";
-}
 
 $nieuwLiedje = new Liedje("dit is de titel", "dit is de artiest");
 
